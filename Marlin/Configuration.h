6a038c07bb494e9c2f6957a6621f514a1568cd78
bstiq <qlebastard@gmail.com>
2021-11-19 12:11:50 +0100

    fix esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 197.36 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98 }
 
 /**
  * Default Max Feed Rate (mm/s)
